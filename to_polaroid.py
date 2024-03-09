373de3dd0ed8da0825e172a4bf6a194608a7959f
Recep Battal <recep.buss@gmail.com>
2024-03-09 23:57:32 +0300

    lines_from_another_files_integrated_here
    added main and collected the adjustments I made in other files in one file

--- to_polaroid.py
+++ to_polaroid.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import os
 
 from pixel_dimensions import *
+from image_cut import *
 
-def apply_polaroid_effect(input_path, output_folder, polaroid_size, frame_sizes=(400, 100, 400, 250)):
+def apply_polaroid_effect(input_path, output_folder, polaroid_size, frame_sizes=(150, 100, 150, 250)):
     #right, top, left, bottom frame sizes
 
     image = Image.open(input_path)#resmi açar
@@ -14,11 +15,21 @@ def apply_polaroid_effect(input_path, output_folder, polaroid_size, frame_sizes=
     timestamp = datetime.now().strftime("%H%M%S")
 
     output_filename = os.path.join(output_folder, f"output_polaroid_{timestamp}.jpg")#resmin adını oluşturur
-    polaroid_image.save(output_filename)#resmi kaydeder
+    polaroid_image.save(output_filename)#resmi kaydede
+    print(f"Polaroid image saved to: {output_filename}")
 
-input_path = "reverend mother 2.jpg"
-output_folder = "output_folder"
-width, height = get_image_dimensions(input_path)
-image_dimensions = (width, height)
+if __name__ == "__main__":
+    input_image = "reverend mother 2.jpg"
+    cropped_folder = "cropped_folder"
+    if not os.path.exists(cropped_folder):
+        os.makedirs(cropped_folder)
 
-apply_polaroid_effect(input_path, output_folder, image_dimensions)
+    input_path = crop_and_resize_image(input_image, cropped_folder)
+    output_folder = "output_folder"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    width, height = get_image_dimensions(input_path)
+    image_dimensions = (width, height)
+    apply_polaroid_effect(input_path, output_folder, image_dimensions)
+
