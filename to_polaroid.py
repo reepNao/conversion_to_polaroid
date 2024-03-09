ecdfc85c0d55ae028bf105e57abc0b0a4a0218ef
Recep Battal <recep.buss@gmail.com>
2024-03-09 23:30:21 +0300

    the_first_attempt2convert2polaroid

---
+++ to_polaroid.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageOps
+from datetime import datetime
+import os
+
+from pixel_dimensions import *
+
+def apply_polaroid_effect(input_path, output_folder, polaroid_size, frame_sizes=(400, 100, 400, 250)):
+    #right, top, left, bottom frame sizes
+
+    image = Image.open(input_path)#resmi açar
+    grayscale_image = image.convert("L")#resmi gri tonlamalı yapar
+    polaroid_image = ImageOps.expand(grayscale_image, border=frame_sizes, fill='white')#beyaz arka plana sahip polaroid resmi oluşturur
+    polaroid_image = polaroid_image.resize(polaroid_size)#tekrar boyutlandırır
+    timestamp = datetime.now().strftime("%H%M%S")
+
+    output_filename = os.path.join(output_folder, f"output_polaroid_{timestamp}.jpg")#resmin adını oluşturur
+    polaroid_image.save(output_filename)#resmi kaydeder
+
+input_path = "reverend mother 2.jpg"
+output_folder = "output_folder"
+width, height = get_image_dimensions(input_path)
+image_dimensions = (width, height)
+
+apply_polaroid_effect(input_path, output_folder, image_dimensions)
